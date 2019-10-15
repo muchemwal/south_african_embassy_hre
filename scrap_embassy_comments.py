55e1deb9aa9d6e0b09665fe3407a417eb369a288
muchemwal <53367688+muchemwal@users.noreply.github.com>
2019-10-15 06:46:00 +0200

    Update scrap_embassy_comments.py

--- scrap_embassy_comments.py
+++ scrap_embassy_comments.py
@@ -11,7 +11,7 @@ client = bigquery.Client()
 
 #command to create a structure of csv file in which we will populate our scraped data
 
-with open('c:\\netone\\South_Africa_Embasy_in_Harare.csv', mode='w') as csv_file:
+with open('c:\\embassy\\South_Africa_Embasy_in_Harare.csv', mode='w') as csv_file:
     fieldnames = ['Title', 'Para', 'Author', 'Date']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
@@ -32,23 +32,23 @@ def scrap_comms(webpage, page_number):
     soup_title = soup.findAll("div",{"class": "comment-username"})
     soup_para = soup.findAll("div",{"class": "comment-content"})
     soup_date = soup.findAll("div",{"class": "comment-date"})
-
+    
     for x in range(len(soup_title)):
         article_author.append(soup_title[x].text.strip())
         article_date.append(soup_date[x].text.strip())
-        page_no.append(page_no)
-        #article_link.append(soup_title[x].a['href'])
-        #missning or blank titles
+        # page_no.append(page_no)
+        # article_link.append(soup_title[x].a['href'])
+        # missing or blank titles
         if '<b>' in str(soup_para[x]):
             article_title.append(soup_para[x].b.text.strip())
             article_para.append(soup_para[x].text.split(soup_para[x].b.text)[1].strip())
         else:
             article_title.append("")
             article_para.append(soup_para[x].text)
-
+    
 #Generating the next page url
-
-    if page_number < 942:
+    
+    if page_number < 100:
         page_number = page_number + 1
         scrap_comms(webpage, page_number)
 
@@ -57,14 +57,11 @@ def scrap_comms(webpage, page_number):
 scrap_comms('https://embassy-finder.com/south-africa_in_harare_zimbabwe?page={}#comments', 0)
 
 #creating the data frame and load a Bigquery table create in setp 1
-data = {'Article_Title':article_title, 'Article_Para':article_para, 'Article_Author':article_author, 'Article_Date':article_date, 'Page_No':page_no}
+data = {'Article_Date':article_date, 'Article_Author':article_author, 'Article_Title':article_title, 'Article_Para':article_para} # , 'Page_No':page_no}
 
-df = DataFrame(data, columns = ['Article_Title','Article_Para','Article_Author','Article_Date','Page_No'])
+df = DataFrame(data, columns = ['Article_Date','Article_Author','Article_Title','Article_Para']) #,'Page_No'])
 
 # write to BQ
 dataset_ref = client.dataset('embasy_pipeline')
-
-client.load_table_from_dataframe(df, dataset_ref)
-
-# To cv
-#df.to_csv(r'c:\\netone\\South_Africa_Embasy_in_Harare.csv', quoting=csv.QUOTE_NONNUMERIC)
+table_ref = dataset_ref.table('stg_sa_emb_hre_comm')
+client.load_table_from_dataframe(df, table_ref)
